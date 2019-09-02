dea95ff91983ff4466af7175c3897339d3bc2b80
Albert Viilik <albert.viilik@gmail.com>
2019-09-02 21:24:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  c = 1
+  while c <= 5
+    c = c + 1
+    if c % 2 == 0
+      puts "-----------"
+    elsif c % 2 == 1
+      puts "   |   |   "
+    end
+  end
